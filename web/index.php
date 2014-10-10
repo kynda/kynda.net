28203a59654977e5a935dd7bf2d6cad80df88bc4
Joseph Hallenbeck <joseph@44interactive.com>
2014-10-10 17:45:59 -0500

    New Kynda.net Landing Page

---
+++ web/index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * HTML Head Element
+ *    
+ * @copright 2014 Joseph Hallenbeck
+ */
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+    <link rel="stylesheet" type="text/css" href="/css/style.css" media="all" />        
+    <link rel="stylesheet" href="/vendors/components-font-awesome/css/font-awesome.min.css">
+    <link href="/media/favicon.ico" rel="icon" type="image/x-icon">
+    
+    <title>Kynda.net - The Works of Joe Hallenbeck</title>
+</head>
+
+<body>
+    
+    <div class="container">
+        <div class="col-xs-12 text-right">
+            <h1>Joseph Hallenbeck</h1>
+            
+            <nav>
+                <ul class="list-inline">
+                    <li><a href="http://joehallenbeck.com" target="_blank"><i class="fa fa-home"></i></a></li>                    
+                    <li><a href="http://joehallenbeck.com/hallenbeck_resume_2014.pdf"><i class="fa fa-file-pdf-o"></i></a></li>
+                    <li><a href="http://wind-up.kynda.net" target="_blank"><i class="fa fa-wordpress"></i></a></li>
+                    <li><a href="https://www.linkedin.com/pub/joseph-hallenbeck" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="https://twitter.com/HallenbeckJoe" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://github.com/Kynda" target="_blank"><i class="fa fa-github"></i></a></li>
+                </ul>
+            </nav>
+            
+        </div>
+    </div>
+
+</body>
