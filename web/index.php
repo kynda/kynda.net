6d136daed3626282f305e63d8919037728d1e9fb
Joseph Hallenbeck <joe@joehallenbeck.com>
2017-07-27 19:51:00 -0400

    Updated links for 2017 Re-Launch

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * HTML Head Element
- *    
+ *
  * @copright 2014 Joseph Hallenbeck
  */
 ?>
@@ -12,64 +12,64 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-    <link rel="stylesheet" type="text/css" href="/css/style.css" media="all" />        
+
+    <link rel="stylesheet" type="text/css" href="/css/style.css" media="all" />
     <link rel="stylesheet" href="/vendors/components-font-awesome/css/font-awesome.min.css">
     <link href="/media/favicon.ico" rel="icon" type="image/x-icon">
-    
+
     <script src="/vendors/jquery/dist/jquery.min.js"></script>
-    <script src="/vendors/bootstrap/js/tooltip.js"></script>    
-    
+    <script src="/vendors/bootstrap/js/tooltip.js"></script>
+
     <title>Kynda.net - The Works of Joe Hallenbeck</title>
 </head>
 
 <body>
-    
+
     <div class="container">
         <div class="col-xs-12 text-right">
             <h1>Joseph Hallenbeck</h1>
-            
+
             <nav>
                 <ul class="list-inline">
                     <li>
-                        <a data-toggle="tooltip" 
-                           data-placement="bottom" 
-                           href="http://joehallenbeck.com" 
+                        <a data-toggle="tooltip"
+                           data-placement="bottom"
+                           href="http://joehallenbeck.com"
                            target="_blank"
-                           title="My dev blog and portfolio">
+                           title="Ye Olde Blog">
                             <i class="fa fa-home"></i>
                         </a>
-                    </li>                    
+                    </li>
                     <li>
-                        <a data-toggle="tooltip" 
-                           data-placement="bottom" 
-                           href="http://joehallenbeck.com/hallenbeck_resume_2014.pdf"
+                        <a data-toggle="tooltip"
+                           data-placement="bottom"
+                           href="http://dreamscapes.kynda.net"
                            target="_blank"
-                           title="Skip straight to my CV">
-                            <i class="fa fa-file-pdf-o"></i>
+                           title="The Dreamscapes Webcomic (on hiatus)">
+                            <i class="fa fa-moon-o"></i>
                         </a>
                     </li>
                     <li>
-                        <a data-toggle="tooltip" 
-                           data-placement="bottom" 
-                           href="http://wind-up.kynda.net" 
+                        <a data-toggle="tooltip"
+                           data-placement="bottom"
+                           href="http://bowlich.com"
                            target="_blank"
-                           title="Musings on literature, film, and games">
-                            <i class="fa fa-wordpress"></i>
+                           title="Bowlich's Land of Links: A link sharing site">
+                            <i class="fa fa-star-o"></i>
                         </a>
                     </li>
                     <li>
-                        <a data-toggle="tooltip" 
-                           data-placement="bottom" 
-                           href="https://www.linkedin.com/pub/joseph-hallenbeck" 
+                        <a data-toggle="tooltip"
+                           data-placement="bottom"
+                           href="https://www.linkedin.com/pub/joseph-hallenbeck"
                            target="_blank"
                            title="My LinkedIn Account">
                             <i class="fa fa-linkedin-square"></i>
                         </a>
                     </li>
                     <li>
-                        <a data-toggle="tooltip" 
-                           data-placement="bottom" 
+                        <a data-toggle="tooltip"
+                           data-placement="bottom"
                            href="https://twitter.com/HallenbeckJoe" t
                            arget="_blank"
                            title="My Twitter Account">
@@ -77,9 +77,9 @@
                         </a>
                     </li>
                     <li>
-                        <a data-toggle="tooltip" 
-                           data-placement="bottom" 
-                           href="https://github.com/Kynda" 
+                        <a data-toggle="tooltip"
+                           data-placement="bottom"
+                           href="https://github.com/Kynda"
                            target="_blank"
                            title="My Github Account">
                             <i class="fa fa-github"></i>
@@ -87,10 +87,10 @@
                     </li>
                 </ul>
             </nav>
-            
+
         </div>
     </div>
-    
+
     <script src="/js/main.js"></script>
 
 </body>
