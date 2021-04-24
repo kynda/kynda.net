8d7eed08f9dc7755910c003a96ae24fa0d9757fb
Joseph Hallenbeck <joe@joehallenbeck.com>
2021-04-23 23:37:22 -0700

    Vickie the Wonder Dog

--- web/index.php
+++ web/index.php
@@ -14,84 +14,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="/css/style.css" media="all" />
-    <link rel="stylesheet" href="/vendors/components-font-awesome/css/font-awesome.min.css">
     <link href="/media/favicon.ico" rel="icon" type="image/x-icon">
 
-    <script src="/vendors/jquery/dist/jquery.min.js"></script>
-    <script src="/vendors/bootstrap/js/tooltip.js"></script>
-
-    <title>Kynda.net - The Works of Joe Hallenbeck</title>
+    <title>Vickie the Wonder Dog!</title>
 </head>
 
 <body>
 
     <div class="container">
-        <div class="col-xs-12 text-right">
-            <h1>Joseph Hallenbeck</h1>
-
-            <nav>
-                <ul class="list-inline">
-                    <li>
-                        <a data-toggle="tooltip"
-                           data-placement="bottom"
-                           href="http://joehallenbeck.com"
-                           target="_blank"
-                           title="Ye Olde Blog">
-                            <i class="fa fa-home"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-toggle="tooltip"
-                           data-placement="bottom"
-                           href="http://dreamscapes.kynda.net"
-                           target="_blank"
-                           title="The Dreamscapes Webcomic (on hiatus)">
-                            <i class="fa fa-moon-o"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-toggle="tooltip"
-                           data-placement="bottom"
-                           href="http://bowlich.com"
-                           target="_blank"
-                           title="Bowlich's Land of Links: A link sharing site">
-                            <i class="fa fa-star-o"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-toggle="tooltip"
-                           data-placement="bottom"
-                           href="https://www.linkedin.com/pub/joseph-hallenbeck"
-                           target="_blank"
-                           title="My LinkedIn Account">
-                            <i class="fa fa-linkedin-square"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-toggle="tooltip"
-                           data-placement="bottom"
-                           href="https://twitter.com/HallenbeckJoe" t
-                           arget="_blank"
-                           title="My Twitter Account">
-                            <i class="fa fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a data-toggle="tooltip"
-                           data-placement="bottom"
-                           href="https://github.com/Kynda"
-                           target="_blank"
-                           title="My Github Account">
-                            <i class="fa fa-github"></i>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
+        <div class="col-xs-12 text-center">
+            <h1>Vickie the Wonder Dog!</h1>
+            <h2>October 6, 2006 - April 23, 2021</h2>
         </div>
     </div>
-
-    <script src="/js/main.js"></script>
 
 </body>
 
